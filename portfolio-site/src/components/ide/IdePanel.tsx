2c8384124958d2f5086741adecb90c24f1d03f1d
joshGrocock <Joshgrocock96@gmail.com>
2022-08-17 19:46:14 +0100

    Update IdePanel.tsx
    template div now stays in center of image

--- portfolio-site/src/components/ide/IdePanel.tsx
+++ portfolio-site/src/components/ide/IdePanel.tsx
@@ -13,50 +13,47 @@ import styled from "styled-components";
 //]);
 
 const IdePanelContainer = styled.div`
-    width: 100vw;
-    height: 100vh;
+
 `;
 
 const DummyImageContainer = styled.div`
-    position: relative;
+    position: absolute;    
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
 `;
 
 const ButtonPanelContainer = styled.div`
-    position: relative;
-    top: 0;
-    left: 0;
+
 `;
 
 const ImageInterior = styled.div`
     top: 10%;
+    left: 6%;
     width: 87.5%;
     background-color: black;
     display: flex;
     justify-content: center;
     align-items: center;
     width: 87.5%;
+    height: 80%;
     position: absolute;
 `;
 
 const DummyImage = styled.img`
-    //background-image: url("/Ide.svg");
+
 `;
 
 export const IdePanel: FunctionComponent = () => {
     return(
-        <IdePanelContainer>
-            <DummyImageContainer>
-                <DummyImage src="/Ide.svg"/>
-                <ImageInterior>
-                    <ButtonPanelContainer>
-                        <IdeButtonPanel />
-                    </ButtonPanelContainer>
-                    <Ide />
-                </ImageInterior>
-            </DummyImageContainer>
-        </IdePanelContainer>
+        <DummyImageContainer>
+            <DummyImage src="/Ide.svg"/>
+            <ImageInterior>
+                <ButtonPanelContainer>
+                    <IdeButtonPanel />
+                </ButtonPanelContainer>
+                <Ide />
+            </ImageInterior>
+        </DummyImageContainer>
     )
 }
